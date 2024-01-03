f2f444f6f7b226938a0803766b7fd4a76f341bd8
Arjunan Ravi <arjunanatwork@gmail.com>
2024-01-03 14:23:27 +0400

    Add global exception handler to accounts service
    Introduced a general exception handler in the GlobalExceptionHandler class. This newly added feature would be used for managing any unexpected exceptions. It mainly consists of creating an "ErrorResponseDTO" object with appropriate error details and returning it as a response entity with "INTERNAL_SERVER_ERROR" status.

--- accounts/src/main/java/org/personal/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/org/personal/accounts/exception/GlobalExceptionHandler.java
@@ -12,6 +12,18 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGlobalException(Exception exception,
+                                                                            WebRequest webRequest){
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException exception,
                                                                                  WebRequest webRequest){
